3b739743d74b924f991d73170c828ca7bb549777
owellmartins <wellmartins38@gmail.com>
2023-07-01 09:49:42 -0300

    Inicial

---
+++ ScreenSound03-projeto-inicial/ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -0,0 +1,40 @@
+﻿using OpenAI_API;
+using ScreenSound.Modelos;
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarBanda : Menu
+{
+	public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+	{
+		base.Executar(bandasRegistradas);
+		ExibirTituloDaOpcao("Registro das bandas");
+		Console.Write("Digite o nome da banda que deseja registrar: ");
+		string nomeDaBanda = Console.ReadLine()!;
+		Banda banda = new Banda(nomeDaBanda);
+
+		if (bandasRegistradas.ContainsKey(nomeDaBanda))
+		{
+			Console.WriteLine($"A banda {nomeDaBanda} já está registrada!");
+			Console.WriteLine("Digite qualquer tecla para voltar ao menu principal.");
+			Console.ReadKey();
+			Console.Clear();
+		}
+		else
+		{
+			bandasRegistradas.Add(nomeDaBanda, banda);
+		}
+
+		var client = new OpenAIAPI("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");
+		var chat = client.Chat.CreateConversation();
+		chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} em 1 parágrafo. Adote um estilo informal.");
+		var resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+		banda.Resumo = resposta;
+
+		Console.WriteLine($"\nA banda {nomeDaBanda} foi registrada com sucesso!");
+		Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+		Console.ReadKey();
+		Console.Clear();
+
+	
+	}
+}
---
+++ ScreenSound03-projeto-inicial/ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,25 @@
+﻿namespace ScreenSound.Modelos;
+
+internal class Avaliacao
+{
+	public Avaliacao(int nota)
+	{
+		Nota = nota;	
+	}
+
+	public int Nota { get; }
+
+	public static Avaliacao Parse(string texto) //o static significa que podemos chamar esse método direto sem precisar chamar o new toda vez que for usá-lo, ou seja, o que está sendo executado aqui dentro do static não usa nenhuma informação da instância dessa classe Avaliacao, mas gera um objeto no método através do return new Avaliacao, ou seja, ele usa em algum momento as instâncias da classe para retornar um objeto. 
+	{
+		int nota = int.Parse(texto);
+		if (nota < 0)
+		{
+			return new Avaliacao(0);
+		}
+		if (nota > 10)
+		{
+			return new Avaliacao(10);
+		}
+		return new Avaliacao(nota);
+	}
+}
